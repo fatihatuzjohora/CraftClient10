1051159f63c2397d6e298bd1c96545b3013ec696
fatihatuzjohora <fatihatuzjohora@gmail.com>
2024-04-30 02:13:57 +0600

    my card done

--- src/Components/Navber/MyCraft.jsx
+++ src/Components/Navber/MyCraft.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 import { AuthContext } from "../Firebase/AuthProvider";
 import { Helmet } from "react-helmet";
@@ -61,72 +61,81 @@ const MyCraft = () => {
   };
 
   return (
-    
     <div>
       <Helmet>
-          <title>My Craft</title>
-        </Helmet>
-        <h1 className="text-4xl text-center font-bold">My Art & Craft</h1>
-        <select className="select select-primary mt-5 mb-5 w-full max-w-xs">
-  <option disabled hidden selected>Select Your Coustomization</option>
-  <option>Yes</option>
-  <option>No</option>
-  
-</select>
-        
+        <title>My Craft</title>
+      </Helmet>
+      <h1 className="text-4xl text-center font-bold">My Art & Craft</h1>
+      <select className="select select-primary mt-5 mb-5 w-full max-w-xs">
+        <option disabled hidden selected>
+          Select Your Coustomization
+        </option>
+        <option>Yes</option>
+        <option>No</option>
+      </select>
+
       <div className=" mt-10 grid grid-cols-1 md:grid-cols-3 gap-5">
+        {email?.map((item) => {
+          return (
+            <div key={item._id}>
+              <div className=" card bg-base-100 shadow-xl p-6 border ">
+                <div className="">
+                  <div className="">
+                    <figure>
+                      <img
+                        data-aos="zoom-in-down"
+                        data-aos-duration="1700"
+                        className="rounded-xl w-full"
+                        src={item.photo}
+                        alt="image"
+                      />
+                    </figure>
+                  </div>
+                  <div className="">
+                    <h2 className="mt-3 text-3xl font-bold">
+                      {" "}
+                      {item.craftName}
+                    </h2>
+                    <h2 className="mt-3 text-3xl font-bold">
+                      {" "}
+                      {item.subcategoryName}
+                    </h2>
+                    <p className="mt-3 text-xl text-slate-600 font-semibold">
+                      shortDescription
+                    </p>
+                    <h1 className="text-2xl mt-3 font-semibold">
+                      {item.price}
+                    </h1>
 
-        
-{email?.map((item) => {
-  return (
-    <div key={item._id}>
-      <div className=" card bg-base-100 shadow-xl p-6 border ">
-        <div className="">
-          <div className="">
-            <figure>
-              <img
-                data-aos="zoom-in-down"
-                data-aos-duration="1700"
-                className="rounded-xl w-full"
-                src={item.photo}
-                alt="image"
-              />
-            </figure>
-          </div>
-          <div className="">
-            <h2 className="mt-3 text-3xl font-bold"> {item.craftName}</h2>
-            <h2 className="mt-3 text-3xl font-bold">
-              {" "}
-              {item.subcategoryName}
-            </h2>
-            <p className="mt-3 text-xl text-slate-600 font-semibold">
-              shortDescription
-            </p>
-            <h1 className="text-2xl mt-3 font-semibold">{item.price}</h1>
+                    <div className="flex gap-5 mt-3">
+                      <Link to={`/ditels/${item._id}`}>
+                        <button className="btn bg-blue-400">View</button>
+                      </Link>
 
-            <div className="flex gap-5 mt-3">
-              <Link to={`/ditels/${item._id}`}>
-                <button className="btn bg-blue-400">View</button>
-              </Link>
+                      <Link to={`/updatecraft/${item._id}`}>
+                        <button className="btn bg-green-400">Updata</button>
+                      </Link>
 
-              <Link to={`/updatecraft/${item._id}`}>
-                <button className="btn bg-green-400">Updata</button>
-              </Link>
-
-              <button
-                onClick={() => handeldelete(item._id)}
-                className="btn bg-red-400"
-              >
-                Delete
-              </button>
+                      <button
+                        onClick={() => handeldelete(item._id)}
+                        className="btn bg-red-400"
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
-    </div>
-  );
-})}
-</div>
+
+      <div className="  block w-40 mx-auto mt-5 mb-5">
+        <Link to={"/addcraft"}>
+          <button className="btn text-2xl font-semibold">Add Craft</button>
+        </Link>
+      </div>
     </div>
   );
 };
