d910c32903f1639dc222706d9d2b8b61bd291559
fatihatuzjohora <fatihatuzjohora@gmail.com>
2024-04-30 18:39:27 +0600

    extra route done

--- src/Components/ExtraRoute/ArtExtra.jsx
+++ src/Components/ExtraRoute/ArtExtra.jsx
@@ -1,41 +1,45 @@
 import { useEffect, useState } from "react";
-import { Link, useLoaderData } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 const ArtExtra = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/art")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
 
+  console.log(data);
 
-const  [data , setData] = useState([])
-useEffect(()=>{
-    fetch('http://localhost:5000/art')
-    .then(response => response.json())
-    .then(json => setData(json))
-},[])
-
-
-
-console.log(data);
-
-    return (
-       <div>
-        <h1 className="text-4xl font-semibold text-center mt-5 mb-5">Art & Craft Categories</h1>
-         <div className="mt-10 grid grid-cols-1 md:grid-cols-3 gap-5">
-       {
-        data.map(item=>{
-            return  <div key={item._id} className="card shadow-md w-96 border text-primary-content">
-            <div className="card-body">
-              <img className="h-[200px]" src={item.photo} alt="" />
-              <p className="text-neutral-700 font-semibold ml-5">{item.subcategoryName}</p>
-              <div className="card-actions justify-center">
-                <Link to={`/artcradtex/${item._id}`}><button className="btn bg-slate-400">Touch Me</button></Link>
+  return (
+    <div>
+      <h1 className="text-4xl font-semibold text-center mt-5 mb-5">
+        Art & Craft Categories
+      </h1>
+      <div className="mt-10 grid grid-cols-1 md:grid-cols-3 gap-5">
+        {data.map((item) => {
+          return (
+            <div
+              key={item._id}
+              className="card shadow-md w-96 border text-primary-content"
+            >
+              <div className="card-body">
+                <img className="h-[200px]" src={item.photo} alt="" />
+                <p className="text-neutral-700 font-semibold ml-5">
+                  {item.subcategoryName}
+                </p>
+                <div className="card-actions justify-center">
+                  <Link to={`/artcradtex/${item._id}`}>
+                    <button className="btn bg-slate-400">Touch Me</button>
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        })
-       }
-        </div>
-       </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ArtExtra;
